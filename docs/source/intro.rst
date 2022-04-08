3189d690135862fa13a55ed5c799417fa5f71eef
Tian Bo <tianb03@hotmail.com>
2022-04-08 10:49:26 +0800

    add virtual leader, two robots nav. improve syntax in leader follower

--- docs/source/intro.rst
+++ docs/source/intro.rst
@@ -1,4 +1,4 @@
 Introduction
 =====
 
-ABC Swarm is the developer manual for ROS2GO, Tianbot Mini and RoboMaster TT ROS Swarm Kit.
+ABC Swarm is an open source project short for Ant Bee Cooperative Swarm. This project aims to use low-cost  robots and drones to achieve multi-robot experiment sulotion without external positioning system. The overall objective of this framework is to support multi-robot coordination, air-ground cooperative swarm, formation control, ant colony optimization, wolf pack algorithm, distributed optimization, reinforcement deep learning, etc. 
--- nodes/leader_follower.py
+++ nodes/leader_follower.py
@@ -22,20 +22,20 @@ pid_angular.output_limits = (-1.5, 1.5)
 k_1 = 1
 k_2 = 1
 d = 0.1
-w_L = 0
-v_L = 0
+w_leader = 0
+v_leader = 0
 
 
 
 def pid_cb(config, level):
     rospy.loginfo("""Reconfigure Request: {linear_kp}, {linear_ki}, {linear_kd}, {angular_kp}, {angular_ki}, {angular_kd}""".format(**config))
-    pid_linear.tunings = [float(config.get(key)) for key in ['linear_kp', 'linear_ki', 'linear_kd']]
-    pid_angular.tunings = [float(config.get(key)) for key in ['angular_kp','angular_ki','angular_kd']]
+    # pid_linear.tunings = [float(config.get(key)) for key in ['linear_kp', 'linear_ki', 'linear_kd']]
+    # pid_angular.tunings = [float(config.get(key)) for key in ['angular_kp','angular_ki','angular_kd']]
     return config
 
 def odom_cb(msg):
-    w_L = msg.twist.twist.angular.z
-    v_L = msg.twist.twist.linear.x
+    w_leader = msg.twist.twist.angular.z
+    v_leader = msg.twist.twist.linear.x
     
 
 if __name__ == '__main__':
@@ -45,15 +45,15 @@ if __name__ == '__main__':
     target_frame = rospy.get_param('~target_frame',leader_robot_name+"/base_link")
     theta0 = rospy.get_param('~expected_theta', np.pi)
     L0 = rospy.get_param('~expected_distance', 1)
-
-    leader_vel = rospy.Subscriber(leader_robot_name + "/odom", Odometry, odom_cb )
+    d = rospy.get_param('~front_distance', 0.1)
+    leader_vel = rospy.Subscriber(leader_robot_name + "/odom", Odometry, odom_cb)
     listener = tf.TransformListener()
 
     follower_vel = rospy.Publisher(follower_robot_name + '/cmd_vel', Twist, queue_size=1)
 
     rate = rospy.Rate(10.0)
 
-    #pid_linear.sample_time = pid_angular.sample_time = 0.1
+    # dynamic_reconfigure
     srv = Server(tf_pidConfig, pid_cb)
 
     while not rospy.is_shutdown():
@@ -67,19 +67,16 @@ if __name__ == '__main__':
         delta_y = trans[1]
         delta_theta = transformations.euler_from_quaternion(rot)[2]
 
-        Err_x = L0 * math.cos(theta0) - delta_x
+        err_x = L0 * math.cos(theta0) - delta_x
         # 纵向误差
-        Err_y = L0 * math.sin(theta0) - delta_y
+        err_y = L0 * math.sin(theta0) - delta_y
 
-        v_F1 = (k_1 * Err_x - delta_y * w_L + v_L) * math.cos(delta_theta) + (k_2 * Err_y + delta_x * w_L) * math.sin(delta_theta)
-        w_F1 = ((k_2 * Err_y + delta_x * w_L) * math.cos(delta_theta) - (k_1 * Err_x - delta_y * w_L + v_L) * math.sin(delta_theta))/d	
-
-        # if abs(linear) < 0.02:
-        #     linear = 0
+        v_follower = (k_1 * err_x - delta_y * w_leader + v_leader) * math.cos(delta_theta) + (k_2 * err_y + delta_x * w_leader) * math.sin(delta_theta)
+        w_follower = ((k_2 * err_y + delta_x * w_leader) * math.cos(delta_theta) - (k_1 * err_x - delta_y * w_leader + v_leader) * math.sin(delta_theta))/d	
 
         msg = Twist()
-        msg.linear.x = v_F1
-        msg.angular.z = w_F1
+        msg.linear.x = v_follower
+        msg.angular.z = w_follower
         follower_vel.publish(msg)
 
         rate.sleep()
